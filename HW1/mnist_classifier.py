ada78ae76aab0ad2d62ff0626fabb593fc9f807c
Paniz <paniz.abedin@gmail.com>
2019-02-11 11:27:05 -0500

    argmax added

--- HW1/mnist_classifier.py
+++ HW1/mnist_classifier.py
@@ -14,12 +14,32 @@ def classify(weight, bias, input_image):
 
 def binary_classify(weight, bias, input_image):
     predicts = classify(weight, bias, input_image)
+    print(predicts.shape)
     for i in range(predicts.shape[1]):
         if predicts.T[i] > 0.5:
             predicts.T[i] = 1
         else:
             predicts.T[i] = 0
     return predicts
+
+
+def ten_digits_classifier(weights, biases, input_image):
+    if input_image.ndim < 2:
+        number_of_inputs = 1
+    else:
+        number_of_inputs = len(input_image)
+
+    probabilities = np.zeros((len(weights), number_of_inputs))
+
+    predictions = np.zeros(number_of_inputs)
+
+    for i in range(0, len(weights)):
+        probabilities[i] = classify(weights[i], biases[i], input_image)
+
+    for i in range(0, number_of_inputs):
+        predictions[i] = np.argmax(probabilities.T[i])
+
+    return predictions
 
 
 def logistic_regression_batch(train_images, train_labels, epochs, lr, target_label):
@@ -101,10 +121,11 @@ def logistic_regression_mini_batch_sgd_loss_se(train_images, train_labels, epoch
             yi = train_labels_shuffled[i:i + batch_size]
             z = np.dot(weight.T, xi.T) + bias
             activation = sigmoid(z)
-            gradient_w = 1.0 / m * (((activation - yi) * (activation*(1 - activation))).dot(xi))
-            gradient_b = 1.0 / m * (np.sum((activation - yi)*(activation * (1-activation))))
-            weight = weight - lr * gradient_w.T
-            bias = bias - lr * gradient_b.T
+            b = (activation - yi) * activation * (1 - activation)
+            gradient_w = 1.0 / m * np.dot(xi.T, b.T)
+            gradient_b = 1.0 / m * np.sum(b)
+            weight = weight - lr * gradient_w
+            bias = bias - lr * gradient_b
 
     return weight, bias
 
@@ -132,9 +153,15 @@ def calculate_accuracy(weight, bias, test_images, test_labels, target_label):
                 false_positive_count += 1
             else:
                 correct_count += 1
-    print(correct_count)
-    print(len(test_labels))
     return correct_count / len(test_labels)
+
+
+def ten_digits_accuracy(weights, biases, test_images, test_labels):
+    test_predictions = ten_digits_classifier(weights, biases, test_images)
+
+    incorrect_count = np.count_nonzero(test_labels - test_predictions)
+
+    return 1.0 - incorrect_count / len(test_labels)
 
 
 def new_accuracy(weight, bias, test_images, test_labels, target_label):
@@ -159,27 +186,35 @@ def mnist_classifier():
 
     test_images = test_images_original.reshape((10000, 28 * 28))
     test_images = test_images.astype('float32') / 255
+
+    # store weights and biases for each classifier
+    weights = []
+    biases = []
+
     for i in range(10):
         target_number = i
 
-        weight, bias = logistic_regression_mini_batch_sgd_loss_ce(train_images, train_labels_original, 100, 0.1,
+        weight, bias = logistic_regression_mini_batch_sgd_loss_ce(train_images, train_labels_original, 100, 1,
                                                                   target_number, 128)
+        weights.append(weight)
+        biases.append(bias)
 
-        print("training set: new accuracy for number ", target_number, ": ",
-              new_accuracy(weight, bias, train_images, train_labels_original, target_number))
-        print("test set: new accuracy for number ", target_number, ": ",
-              new_accuracy(weight, bias, test_images, test_labels_original, target_number))
-
+        print("done classifier: ", i)
+        # print("training set: new accuracy for number ", target_number, ": ",
+        #       new_accuracy(weight, bias, train_images, train_labels_original, target_number))
+        # print("test set: new accuracy for number ", target_number, ": ",
+        #       new_accuracy(weight, bias, test_images, test_labels_original, target_number))
         # print("accuracy for number ", target_number, ": ",
         #       calculate_accuracy(weight, bias, train_images, train_labels_original, target_number))
-        # print("old accuracy for number ", target_number, ": ",
+
+        # print("accuracy for number ", target_number, ": ",
         #       calculate_accuracy(weight, bias, test_images, test_labels_original, target_number))
         # print(train_images.shape)
         # print(train_labels_original.shape)
 
-    # for j in range(1, 1000):
-    #     print("original label: ", test_labels_original[j], ", classified as ", target_number, ": ",
-    #           "yes" if classify(weight, bias, test_images[j]) > .5 else "no")
+    # print("original label: ", test_labels_original[:5], ", classified as ",
+    #       ten_digits_classifier(weights, biases, test_images[:5, :]))
+    print("accuracy: ", ten_digits_accuracy(weights, biases, test_images, test_labels_original) * 100, "%")
 
 
 mnist_classifier()
