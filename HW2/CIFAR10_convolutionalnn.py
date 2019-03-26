493ab448d241339e1df1599e7c10b137a6d7735a
Paniz <paniz.abedin@gmail.com>
2019-03-26 12:18:19 -0400

    Ploting acc

--- HW2/CIFAR10_convolutionalnn.py
+++ HW2/CIFAR10_convolutionalnn.py
@@ -6,20 +6,20 @@ import matplotlib.pyplot as plt
 from keras.utils.np_utils import to_categorical
 
 
+# loading CIFAR10 data
 (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
 
-print(train_images.shape)
-
-print(test_images.shape)
-
+# normalizing data
 train_images = train_images.reshape((50000, 32, 32, 3)) / 255.0
 test_images = test_images.reshape((10000, 32, 32, 3)) / 255.0
 
+# m is the number of training examples
 m = train_images.shape[0]
 shuffled_indices = np.random.permutation(m)
 train_images_shuffled = train_images[shuffled_indices]
 train_labels_shuffled = train_labels[shuffled_indices]
 
+# setting 0.20 of the training data to validation set randomly
 validation_set_size = int(0.2 * 50000)
 
 validation_set = train_images_shuffled[0:validation_set_size]
@@ -42,16 +42,14 @@ training_set_labels = train_labels_shuffled[validation_set_size:]
 #     axarr[i].imshow(img)
 # plt.show()
 
-print(training_set_labels[0])
-
-print(validation_set.shape)
-print(training_set.shape)
 
 
 # one hot encoding of the labels
 training_set_labels = to_categorical(training_set_labels, num_classes=10)
 test_labels = to_categorical(test_labels, num_classes=10)
 validation_set_labels = to_categorical(validation_set_labels, num_classes=10)
+
+print("--------------------First Architecture--------------------")
 
 model = keras.Sequential([
     keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
@@ -68,24 +66,44 @@ model = keras.Sequential([
 ])
 model.summary()
 
+
 model.compile(optimizer='adam',
-             loss='categorical_crossentropy',
-             metrics=['accuracy'])
-
-epochs = 2
-history = model.fit(training_set,
-                      training_set_labels,
-                      epochs=epochs,
-                      validation_data=(validation_set, validation_set_labels))
+                 loss='categorical_crossentropy',
+                 metrics=['accuracy'])
 
 
-def plotLosses(history):
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
+def history(training_data, training_data_labels, validation_data, validation_data_labels, epochs, batch_size):
+    history = model.fit(training_data, training_data_labels,
+                          epochs=epochs, batch_size=batch_size,
+                          validation_data=(validation_data, validation_data_labels))
+    return history
+
+
+
+def plot(history, epochs):
+    history_dict = history.history
+    loss_values = history_dict['loss']
+    test_loss_values = history_dict['val_loss']
+    epochs_range = range(1, epochs + 1)
+    plt.plot(epochs_range, loss_values, 'bo', label='Training loss')
+    plt.plot(epochs_range, test_loss_values, 'ro', label='Test loss')
+    plt.title('Training and test loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
     plt.show()
 
-plotLosses(history)
+    acc_values = history_dict['acc']
+    test_acc_values = history_dict['val_acc']
+    plt.plot(epochs_range, acc_values, 'bo', label='Training accuracy')
+    plt.plot(epochs_range, test_acc_values, 'ro', label='Validation accuracy')
+    plt.title('Training and Validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+plot(history(training_set, training_set_labels, validation_set,validation_set_labels, 10, 64),10)
+
+print("--------------------Second Architecture--------------------")
+
